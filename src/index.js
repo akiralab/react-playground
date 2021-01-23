ee1afc296b11d040413db1691fdd5b93b7b989ab
akiralab <kawaiakira51@gmail.com>
2021-01-23 19:49:11 +0900

    disabled default App.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ const App = () => {
   return <div>Hi there!</div>;
 };
 
-
 ReactDOM.render(<App />, document.querySelector('#root'));
 
 // If you want to start measuring performance in your app, pass a function
