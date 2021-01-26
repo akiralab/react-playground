fcb83113988ec75716c4e7f983cd249494662743
Akira Kawai <akira.kawai@robit.co.jp>
2021-01-26 23:26:45 +0900

    changed avatar

--- src/index.js
+++ src/index.js
@@ -9,13 +9,17 @@ const App = () => {
   return (
     <div className="ui container comments">
       <ApproveCard>
-        <CommentDetail author="うさぎ丸" content="まりちゃん大好き" date="Today at 5:00PM" avatar="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/rabbit-breeds-american-white-1553635287.jpg?crop=0.976xw:0.651xh;0.0242xw,0.291xh&resize=768:*" />
+        <CommentDetail author="うさぎ" content="ぴょんぴょん" date="Today at 5:00PM" avatar="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/rabbit-breeds-american-white-1553635287.jpg?crop=0.976xw:0.651xh;0.0242xw,0.291xh&resize=768:*" />
       </ApproveCard>
       <ApproveCard>
-        <CommentDetail author="ネズミ丸" content="まりちゃん大好き" date="Today at 5:00PM" avatar="https://www.interdependenceday.net/wp-content/uploads/2017/12/afd25f4138e42591d819ebbbf4e4a371_s-e1528703923909.jpg" />
+        <CommentDetail author="ねずみ" content="ちゅんちゅん" date="Today at 5:00PM" avatar="https://www.interdependenceday.net/wp-content/uploads/2017/12/afd25f4138e42591d819ebbbf4e4a371_s-e1528703923909.jpg" />
       </ApproveCard>
-      <CommentDetail author="Akira" content={faker.lorem.sentence()} date="Today at 9:00PM" avatar={faker.image.image()} />
-      <CommentDetail author="Mari" content={faker.lorem.sentence()} date="Today at 11:00PM" avatar={faker.image.image()} />
+      <ApproveCard>
+        <CommentDetail author="さる" content="キーキー" date="Today at 9:00PM" avatar="https://previews.123rf.com/images/yupiramos/yupiramos1707/yupiramos170726256/82984869-cute-bird-drawing-icon-vector-illustration-design.jpg" />
+      </ApproveCard>
+      <ApproveCard>
+        <CommentDetail author="とり" content="ぱたぱた" date="Today at 11:00PM" avatar={faker.image.image()} />
+      </ApproveCard>
     </div>
     );
 };
