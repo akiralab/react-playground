a0a9c202d52d2e9487cda80b0053ac9a9bb01143
akiralab <kawaiakira51@gmail.com>
2021-02-16 22:53:32 +0900

    course 159

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import React from 'react'
-import Accordion from './components/Accordion'
-
-
-const items = [
-    {
-        title: 'What is React?',
-        content: 'React is a front end javascript framework.'
-    },
-    {
-        title: 'What is React Native?',
-        content: 'React Native is a cross-platform javascript framework.'
-    },
-    {
-        title: 'What is Flutter?',
-        content: 'Flutter is a cross-platform Dart framework.'
-    }    
-]
+import React from 'react';
+import Accordion from './components/Accordion';
+import Search from './components/Search';
 
 
 export default () => {
-    return <Accordion items={items}/>;
+    return <Search />;
 }
--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,17 +1,29 @@
 import React, { useState } from 'react';
 
+// const items = [
+//     {
+//         title: 'What is React?',
+//         content: 'React is a front end javascript framework.'
+//     },
+//     {
+//         title: 'What is React Native?',
+//         content: 'React Native is a cross-platform javascript framework.'
+//     },
+//     {
+//         title: 'What is Flutter?',
+//         content: 'Flutter is a cross-platform Dart framework.'
+//     }    
+// ]
+
 const Accordion = ({ items }) => {
     const [ activeIndex, setActiveIndex ] = useState(null);
-
     const onTitleClick = (index) => {
         setActiveIndex(index);
         console.log('Title clicked', index);
     };
 
     const renderedItems = items.map((item, index) => {
-
         const active  = index ===  activeIndex ? 'active' : '';
-
         return (            
             <React.Fragment key={item.title}>
                 <div className={`title ${active}`} onClick={()=>onTitleClick(index)}>
