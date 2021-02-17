72af6392c5e78bf5eedf64ff2d793a0b856d8f4a
akiralab <kawaiakira51@gmail.com>
2021-02-17 23:04:55 +0900

    added Search.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown'
+
+const options = [
+    {
+      label: 'The color is Red',
+      value: 'red'
+    },
+    {
+      label: 'The color is Blue',
+      value: 'blue'
+    },
+    {
+      label: 'The color is Yellow',
+      value: 'yellow'
+    },
+  ];
 
 export default () => {
-    return <Search />;
-}
+    const [selected, setSelected] = useState(options[0]);
+    return(
+    <div>
+        <Dropdown
+            selected={selected}
+            onSelectedChange={setSelected}
+            options={options} />
+    </div>
+    );
+};
