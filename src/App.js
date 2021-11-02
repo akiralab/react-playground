8e1acd5e4a3348c8ea7103fe577b425e8e5cbc24
Akira Kawai <akirakawai@enoMacBook-Pro.local>
2021-11-02 23:07:12 +0900

    old file transferred

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
 import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
-import Dropdown from './components/Dropdown'
+import Dropdown from './components/Dropdown';
 
 const options = [
-    {
-      label: 'The color is Red',
-      value: 'red'
-    },
-    {
-      label: 'The color is Blue',
-      value: 'blue'
-    },
-    {
-      label: 'The color is Yellow',
-      value: 'yellow'
-    },
-  ];
+ {
+  label: 'The color is Red',
+  value: 'red',
+ },
+ {
+  label: 'The color is Blue',
+  value: 'blue',
+ },
+ {
+  label: 'The color is Yellow',
+  value: 'yellow',
+ },
+];
 
 export default () => {
-    const [selected, setSelected] = useState(options[0]);
-    return(
-    <div>
-        <Dropdown
-            selected={selected}
-            onSelectedChange={setSelected}
-            options={options} />
-    </div>
-    );
+ const [selected, setSelected] = useState(options[0]);
+
+ return (
+  <div>
+   <Dropdown
+    selected={selected}
+    onSelectedChange={setSelected}
+    options={options}
+   />
+  </div>
+ );
 };
--- src/old/App.js
+++ src/old/App.js
@@ -1,19 +1,64 @@
-import React from 'react';
-import SearchBar from './SearchBar';
+import React, { useState } from 'react';
+import Accordion from './components/Accordion';
+import Search from './components/Search';
+import Dropdown from './components/Dropdown';
+import Translate from './components/Translate';
+import Route from './components/Route';
+import Header from './components/Header';
 
+const items = [
+ {
+  title: 'What is React?',
+  content: 'React is a front end javascript framework',
+ },
+ {
+  title: 'Why use React?',
+  content: 'React is a favorite JS library among engineers',
+ },
+ {
+  title: 'How do you use React?',
+  content: 'You use React by creating components',
+ },
+];
 
-class App extends React.Component {
+const options = [
+ {
+  label: 'The Color Red',
+  value: 'red',
+ },
+ {
+  label: 'The Color Green',
+  value: 'green',
+ },
+ {
+  label: 'A Shade of Blue',
+  value: 'blue',
+ },
+];
 
-  onSearchSubmit(searchText){
-    console.log(searchText);
-  }
+export default () => {
+ const [selected, setSelected] = useState(options[0]);
 
-  render(){
-    return (
-      <div className="ui container" style={{marginTop: '10px'}}>
-        <SearchBar onSubmit={this.onSearchSubmit} />
-      </div>
-    ) 
-  }
-}
-export default App;
+ return (
+  <div>
+   <Header />
+   <Route path="/">
+    <Accordion items={items} />
+   </Route>
+   <Route path="/list">
+    <Search />
+   </Route>
+   <Route path="/dropdown">
+    <Dropdown
+     label="Select a color"
+     options={options}
+     selected={selected}
+     onSelectedChange={setSelected}
+    />
+   </Route>
+   <Route path="/translate">
+    <Translate />
+   </Route>
+  </div>
+ );
+};
